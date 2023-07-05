ce5a3218ca4d4eca16afdc08278cd85772eff662
Raifell <ArtSkvaer2@gmail.com>
2023-07-05 20:21:00 +0500

    Task_2

--- Task_1/Task_1.py
+++ Task_1/Task_1.py
@@ -1,38 +1,34 @@
-class Man:
+class City:
     def __init__(self):
-        self.name = 'Aden'
-        self.surname = 'Pride'
-        self.birth = '2001.12.10'
-        self.phone = '+1999999999'
-        self.town = 'NY'
-        self.country = 'USA'
-        self.address = 'NY, queens 23 A'
+        self.name = 'Argon'
+        self.region = 'Valarant'
+        self.country = 'Silvermilen'
+        self.count_people = 5000
+        self.index = 159753
+        self.phone_code = 321
+
 
     def get_info(self, choice):
         self.choice = choice
 
         if self.choice == 1:
-            print(self.name, self.surname, self.birth, self.birth, self.phone, self.town, self.country, self.address)
+            print(self.name, self.region, self.country, self.count_people, self.index, self.phone_code)
         elif self.choice == 2:
             print(self.name)
         elif self.choice == 3:
-            print(self.surname)
+            print(self.region)
         elif self.choice == 4:
-            print(self.birth)
-        elif self.choice == 5:
-            print(self.phone)
-        elif self.choice == 6:
-            print(self.town)
-        elif self.choice == 7:
             print(self.country)
-        elif self.choice == 8:
-            print(self.address)
+        elif self.choice == 5:
+            print(self.count_people)
+        elif self.choice == 6:
+            print(self.index)
+        elif self.choice == 7:
+            print(self.phone_code)
 
 
-a = Man()
-
-ch = input('1) - All info\n2) - Name\n3) - Surname\n4) - Birth date\n5) - Phone number\n6) - City\n7) - County\n'
-           '8) - Address\nChoice: ')
+a = City()
+ch = input('1) - All info\n2) - Name\n3) - Region\n4) - Country\n5) - Count people\n6) - Index\n7) - Phone code\n'
+           'Choice: ')
 
 a.get_info(int(ch))
-
