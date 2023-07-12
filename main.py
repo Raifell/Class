cefa4ad4ac508f2c23b819d7ac4318f7b9664580
Raifell <ArtSkvaer2@gmail.com>
2023-07-12 20:07:21 +0500

    start

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('Example')
+count = []
+
+with open('text.txt', 'r') as file:
+    string = file.readlines()
+
+result = '|'.join(string).replace('\n', '')
+over = result.split('|')
+over = over[::-1]
+
+with open('result_text.txt', 'w') as file:
+    for x in over:
+        file.write(x + '\n')
